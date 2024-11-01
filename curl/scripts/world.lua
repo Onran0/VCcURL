590a5cdd7ff8b0e3621df8e68f85bfeaafae11a0
Onran <100285264+Onran0@users.noreply.github.com>
2024-11-01 15:42:57 +0900

    world.lua: debug code removed

--- curl/scripts/world.lua
+++ curl/scripts/world.lua
@@ -1,27 +1,4 @@
 local curl = require "curl:curl"
-local http = require "curl:protocols/http"
-
-local routine = coroutine.create(
-	function()
-		local status, error = pcall(
-			function()
-				print("REQUEST SENDED!!!!!!!!!!!!!!")
-		
-				local info, head = http.get("voxelworld.ru/api/v1/mods",
-					{
-						responseType = "dirty_json",
-						secured = true
-					}
-				)
-
-				debug.print(head)
-				debug.print(info)
-			end
-		)
-
-		if not status then print(error) end
-	end
-)
 
 function on_world_open()
 	curl.initialize()
@@ -30,8 +7,4 @@ end
 
 function on_world_tick()
 	curl.process_responses()
-
-	if coroutine.status(routine) ~= "dead" then
-		coroutine.resume(routine)
-	end
 end
